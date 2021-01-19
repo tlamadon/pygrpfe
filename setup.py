d4c2e200d8d4e8674b246b284b0e44bae2aa1b5d
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2021-01-19 17:54:18 -0600

    new pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='pygrpfe',
-    version='0.1.0',
+    version='0.1.1',
     author='Thibaut Lamadon',
     author_email='thibaut.lamadon@gmail.com',
     description='Helper functions of Group Fixed Effect',
