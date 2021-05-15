e28450cbd3c96788d918b5869a552f1cc2189048
Adam Oppenheimer <adam.a.oppenheimer@gmail.com>
2021-05-15 13:20:12 -0400

    Upload to pip

--- setup.py
+++ setup.py
@@ -1,32 +1,8 @@
-import setuptools
+from setuptools import setup
 
-with open('README.rst', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
+if __name__ == "__main__":
+    setup()
 
-setuptools.setup(
-    name='pygrpfe',
-    version='0.1.1',
-    author='Thibaut Lamadon',
-    author_email='thibaut.lamadon@gmail.com',
-    description='Helper functions of Group Fixed Effect',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/tlamadon/pygrpfe',
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scipy',
-        'matplotlib',
-        'tqdm',
-        'scikit-learn',
-        'torch',
-        'statsmodels'
-      ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+# with open('README.rst', 'r', encoding='utf-8') as fh:
+#     long_description = fh.read()
+
